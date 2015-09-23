02cc3a9e440c0d4201e6d1e3c12712e4f9690652
Andrew Hanes <ahanes@csh.rit.edu>
2015-09-23 12:50:22 -0400

    file read

--- modules/skill.py
+++ modules/skill.py
@@ -4,7 +4,7 @@ Skills echo tool
 __author__ = 'ahanes'
 import json
 def skillcheck(skill=None):
-    skills = json.loads('skills.pickle')
+    skills = json.loads(open('skills.pickle').read())
     if skill in skills and len(skills[skill]) > 0:
         if len(skills[skill]) > 1:
             name = skills[skill][0] + ' and ' + skills[skill][1] + " know " + skill
